8f5764f79d2abdbb6eadd50b1c9a6f0a0d376d2a
Usama Hayat N <116257721+UsamaHayatN@users.noreply.github.com>
2025-08-30 23:51:50 +0500

    Fix error in Sanity-Client

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,12 +1,13 @@
-import { createClient } from "next-sanity";
+import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
   projectId: "dvuiz4o3",
   dataset: "production",
+  apiVersion: "2023-08-30",
   useCdn: true,
-  apiVersion: "2025-08-30",
 });
 
+// Image URL builder
 const builder = imageUrlBuilder(client);
 export const urlFor = (source) => builder.image(source);
